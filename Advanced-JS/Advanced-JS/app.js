5a8aaafe5636631ed40c6a24c4b9bc4800409ed2
vishalkrsinha <42475844+vishalkrsinha@users.noreply.github.com>
2018-09-27 20:45:44 +0530

    Update app.js

--- Advanced-JS/Advanced-JS/app.js
+++ Advanced-JS/Advanced-JS/app.js
@@ -185,3 +185,111 @@ game();
     var score = Math.random() * 10;
     console.log(score >= 5 - goodluck);
 })(5);
+
+
+
+
+/*
+///////////////////////////////////////////////////////////////
+//CLOSURES: An inner function always has access to the variables & parameters of its outer function, even after the outer function has returned.
+///////////////////////////////////////////////////////////////
+*/
+function retirement(retirementAge) {
+    var a = " years left until retirement.";
+    return function (yearOfBirth) {
+        var age = 2018 - yearOfBirth;
+        console.log(retirementAge - age + a);
+    }
+}
+
+var retirementIndia = retirement(60);
+retirementIndia(1982);
+
+//OR
+
+retirement(60)(1982);
+
+/*
+///////////////////////////////////////////////////////////////
+//CLOSURES: Functions returning functions
+///////////////////////////////////////////////////////////////
+*/
+function interviewQuestion(job) {
+    return function (name) {
+            if (job === 'designer')
+                console.log(name + ', can you please explain what UX design is?');
+            else if (job === 'teacher') 
+                console.log('what subject do you teach, ' + name + '?');
+            else 
+                console.log('Hello, ' + name + ', what do you do?');
+        }
+}
+
+interviewQuestion('designer')('Vishal');
+
+/*
+///////////////////////////////////////////////////////////////
+//Bind, call and apply
+///////////////////////////////////////////////////////////////
+*/
+var john = {
+    name: 'john',
+    age: 26,
+    job: 'teacher',
+    presentation: function (style, timeOfDay) {
+        if (style === 'formal')
+            console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' + this.name + ', I\'m ' + this.job + ', and I\'m ' + this.age + ' years old.');
+        else if (style === 'friendly') {
+            console.log('Hey! What\'s up? I\'m ' + this.name + ', I\'m ' + this.job + ', and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay);
+        }
+    }
+};
+
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer',
+};
+
+john.presentation('formal', 'morning');
+
+john.presentation.call(emily, 'friendly', 'afternoon');
+
+//john.presentation.apply(emily, ['friendly', 'afternoon']);
+
+var johnFriendly = john.presentation.bind(john, 'friendly');
+johnFriendly('morning');
+johnFriendly('night');
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+emilyFormal('afternoon');
+
+
+/*
+///////////////////////////////////////////////////////////////
+//Bind, call and apply: Passing functions as arguments
+///////////////////////////////////////////////////////////////
+*/
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2018 - el;
+}
+
+function isFullAge(limit, el) {
+    return el >= limit;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+console.log(ages);
+console.log(fullJapan);
