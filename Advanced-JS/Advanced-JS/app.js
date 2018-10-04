905888f2fcc15c52fcb2a122a54f3b6f601e4581
vishalkrsinha <42475844+vishalkrsinha@users.noreply.github.com>
2018-10-04 19:53:21 +0530

    Update app.js

--- Advanced-JS/Advanced-JS/app.js
+++ Advanced-JS/Advanced-JS/app.js
@@ -327,3 +327,45 @@ c. correct answer (I would use a number for this)
 11. Display the score in the console. Use another method for this. 
 ///////////////////////////////////////////////////////////////
 */
+
+(function () {
+
+
+function Question(question, answers, correctAnswer) {
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+}
+
+var q1 = new Question("Is JavaScript the coolest programming language in the world?", ['Yes', 'No'], 0);
+var q2 = new Question("What is the name of this course\'s teacher?", ['John', 'Michael', 'Jonas'], 2);
+var q3 = new Question("What does best describe coding?", ['Boring', 'Hard', 'Tedious', 'Fun'], 3);
+
+var questions = [q1, q2, q3];
+
+var number = Math.floor(Math.random() * questions.length);
+
+Question.prototype.displayQuestion =
+function()
+{
+    console.log(this.question);
+
+    for (var i = 0; i < this.answers.length; i++) {
+        console.log(i + ": " + this.answers[i]);
+    }
+}
+
+Question.prototype.checkAnswer =
+    function (ans) {
+        if (ans == this.correctAnswer)
+            console.log('Correct answer!');
+        else
+            console.log('Wrong answer. Try again :)');
+    }
+
+questions[number].displayQuestion();
+
+var answer = parseInt(prompt('Please select the correct answer.'));
+
+questions[number].checkAnswer(answer);
+})();
